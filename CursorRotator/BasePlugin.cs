abe28f60d076ddd74301d8f566c03bb2838ec8e2
Denyscrasav4ik <horanden238@gmail.com>
2026-02-18 20:23:38 +0200

    Initial Commit

---
+++ CursorRotator/BasePlugin.cs
@@ -0,0 +1,80 @@
+﻿using System.Linq;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using UnityEngine;
+
+[BepInPlugin("denyscrasav4ik.basicallyukrainian.cursorrotator", "Cursor Rotator", "1.0.1")]
+public class CursorRotatorPlugin : BaseUnityPlugin
+{
+    internal static ConfigEntry<float> MaxDistanceConfig;
+
+    private void Awake()
+    {
+        MaxDistanceConfig = Config.Bind(
+            "General",
+            "MaxDistance",
+            100f,
+            "Maximum distance at which the cursor rotates toward a button."
+        );
+
+        var harmony = new Harmony("denyscrasav4ik.basicallyukrainian.cursorrotator");
+        harmony.PatchAll();
+    }
+}
+
+[HarmonyPatch(typeof(CursorController), "Update")]
+class CursorController_Update_Patch
+{
+    static void Postfix(CursorController __instance)
+    {
+        RotateCursorTowardsClosestButton(__instance);
+    }
+
+    private static void RotateCursorTowardsClosestButton(CursorController cursor)
+    {
+        var buttons = GameObject
+            .FindObjectsOfType<StandardMenuButton>()
+            .Where(b => b.gameObject.activeInHierarchy && b.enabled)
+            .ToArray();
+
+        if (buttons.Length == 0)
+            return;
+
+        float maxDistance = CursorRotatorPlugin.MaxDistanceConfig.Value;
+
+        Vector3 cursorPos = cursor.cursorTransform.position;
+
+        StandardMenuButton closest = null;
+        float closestDist = float.MaxValue;
+
+        foreach (var btn in buttons)
+        {
+            float dist = Vector3.Distance(cursorPos, btn.transform.position);
+
+            if (dist > maxDistance)
+                continue;
+
+            if (dist < closestDist)
+            {
+                closestDist = dist;
+                closest = btn;
+            }
+        }
+
+        if (closest == null)
+        {
+            cursor.cursorTransform.rotation = Quaternion.identity;
+            return;
+        }
+
+        RectTransform rect = closest.GetComponent<RectTransform>();
+        Vector3 worldCenter = rect.TransformPoint(rect.rect.center);
+        Vector3 direction = worldCenter - cursorPos;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+
+        angle -= 90f;
+
+        cursor.cursorTransform.rotation = Quaternion.Euler(0f, 0f, angle);
+    }
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+# Baldi's Basics Plus Cursor Rotator
+
+## 📝 Functionality
+
+Basically, all this mod does is just rotate the cursor towards a button within a certain range.
+
+## 📜 License
+
+This project is shared under the MIT License. Read LICENSE for more info.
